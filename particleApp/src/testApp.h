7839ff0bd0499116cb2944664deec88ac2e32f0f
Krisjanis Rijnieks <krisjanis.rijnieks@gmail.com>
2014-03-14 15:56:16 +0100

    Cleaned particleApp from unused code

--- particleApp/src/testApp.h
+++ particleApp/src/testApp.h
@@ -4,33 +4,22 @@
 #include "myParticle.h"
 #include "ofxMidi.h"
 
-class testApp : public ofBaseApp, public ofxMidiListener{
-	public:
-		void setup();
-		void update();
-		void draw();
+class testApp : public ofBaseApp, public ofxMidiListener
+{
+public:
+	void setup();
+	void update();
+	void draw();
 		
-		void keyPressed(int key);
-		void keyReleased(int key);
-		void mouseMoved(int x, int y);
-		void mouseDragged(int x, int y, int button);
-		void mousePressed(int x, int y, int button);
-		void mouseReleased(int x, int y, int button);
-		void windowResized(int w, int h);
-		void dragEvent(ofDragInfo dragInfo);
-		void gotMessage(ofMessage msg);
-    
-    int numParticles;
-    vector<myParticle> particles;
-    ofPoint speedMult, speedMultMax;
-    float radiusMult, radiusMultMax;
-    
-    ofxMidiIn midiIn;
-    ofxMidiMessage midiMessage;
-    void newMidiMessage(ofxMidiMessage& eventArgs);
-    
-    std::string controllerName;
-    bool controllerFound;
-    
-    void checkController();
+	void newMidiMessage(ofxMidiMessage& eventArgs);
+	void checkController();
+	
+	int numParticles;
+	bool controllerFound;
+	float radiusMult, radiusMultMax;
+	vector<myParticle> particles;
+	ofPoint speedMult, speedMultMax;
+	ofxMidiIn midiIn;
+	ofxMidiMessage midiMessage;
+	string controllerName;
 };
